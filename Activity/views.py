5109da7828d367713b3fcb2aa4e71f6463762645
nesta <maiaproject44@gmail.com>
2021-01-22 09:03:50 +0100

    modif

--- Activity/views.py
+++ Activity/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	mypost=['Act1', 'Act2', 'Act3']
-	return render(request,'activities:index', {'posts': mypost})
+	return render(request,'Activity/index', {'posts': mypost})
